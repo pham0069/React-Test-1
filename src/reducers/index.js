209baf5a90a9ec99fe1f7751613d728e764c2bff
Diep Pham <dpham@flextrade.com>
2022-04-21 17:51:21 +0800

    Add post reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
-import success from './successReducers';
+import posts from './posts/reducer';
 
 
 export default combineReducers({
-    success
+    posts
 });
---
+++ src/reducers/posts/reducer.spec.js
@@ -0,0 +1,32 @@
+import { types } from '../../actions/types';
+import postReducer from './reducer';
+
+describe('Posts reducer', () => {
+    it('Should return default state', () => {
+        const newState = postReducer(undefined, {});
+        expect(newState).toEqual([]);
+    });
+
+    it('Should return new state if receiving type', () => {
+        const posts = [
+            {
+                title: 'Test 1'
+            },
+            {
+                title: 'Test 2'
+            },
+            {
+                title: 'Test 3'
+            },
+        ];
+
+        const newState = postReducer(undefined, {
+            type: types.GET_POSTS,
+            payload: posts
+        });
+
+        expect(newState).toEqual(posts);
+
+
+    });
+});
